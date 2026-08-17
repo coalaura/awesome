91cf358cd88025588a89a2bd0ae7fb4398e736d1
Laura <laura@wiese2.org>
2026-08-17 04:43:46 +0200

    improve markdown extraction

--- markdown.go
+++ markdown.go
@@ -1,6 +1,9 @@
 package main
 
-import "strings"
+import (
+	"iter"
+	"strings"
+)
 
 type MarkdownURL struct {
 	Name string `json:"name"`
@@ -8,99 +11,119 @@ type MarkdownURL struct {
 }
 
 func NewMarkdownURLs(addedLines, removedLines []string) []MarkdownURL {
-	removedURLs := make(map[string]int)
+	removed := make([]MarkdownURL, 0, len(removedLines))
 
 	for _, line := range removedLines {
-		for _, url := range AppendSimpleMarkdownURLs(nil, line) {
-			removedURLs[url.URL]++
+		for url := range FindSimpleMarkdownURLs(line) {
+			removed = append(removed, url)
 		}
 	}
 
-	urls := make([]MarkdownURL, 0)
+	urls := make([]MarkdownURL, 0, len(addedLines))
 
 	for _, line := range addedLines {
-		for _, url := range AppendSimpleMarkdownURLs(nil, line) {
-			if removedURLs[url.URL] > 0 {
-				removedURLs[url.URL]--
+		for added := range FindSimpleMarkdownURLs(line) {
+			if i := IndexMatchingMarkdownURL(removed, added); i >= 0 {
+				removed = append(removed[:i], removed[i+1:]...)
 
 				continue
 			}
 
-			urls = append(urls, url)
+			urls = append(urls, added)
 		}
 	}
 
 	return urls
 }
 
-func AppendSimpleMarkdownURLs(urls []MarkdownURL, input string) []MarkdownURL {
-	var index int
-
-	for index < len(input) {
-		if input[index] == '!' && index+1 < len(input) && input[index+1] == '[' {
-			index += 2
-
-			continue
+func IndexMatchingMarkdownURL(removed []MarkdownURL, added MarkdownURL) int {
+	for i, item := range removed {
+		if item.URL == added.URL {
+			return i
 		}
-
-		if input[index] != '[' {
-			index++
-
-			continue
-		}
-
-		index++
-
-		var (
-			completed bool
-			name      strings.Builder
-		)
-
-		for index < len(input) {
-			if input[index] == ']' {
-				completed = true
-
-				break
-			}
-
-			name.WriteByte(input[index])
-
-			index++
-		}
-
-		index++
-
-		if !completed || name.Len() == 0 || index >= len(input) || input[index] != '(' {
-			continue
-		}
-
-		index++
-
-		var url strings.Builder
-
-		for index < len(input) {
-			if input[index] == ')' {
-				completed = true
-
-				break
-			}
-
-			url.WriteByte(input[index])
-
-			index++
-		}
-
-		index++
-
-		if !completed || url.Len() == 0 {
-			continue
-		}
-
-		urls = append(urls, MarkdownURL{
-			Name: name.String(),
-			URL:  url.String(),
-		})
 	}
 
-	return urls
+	for i, item := range removed {
+		if item.Name == added.Name {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func FindSimpleMarkdownURLs(input string) iter.Seq[MarkdownURL] {
+	return func(yield func(MarkdownURL) bool) {
+		var index int
+
+		for index < len(input) {
+			if input[index] == '!' && index+1 < len(input) && input[index+1] == '[' {
+				index += 2
+
+				continue
+			}
+
+			if input[index] != '[' {
+				index++
+
+				continue
+			}
+
+			index++
+
+			var (
+				completed bool
+				name      strings.Builder
+			)
+
+			for index < len(input) {
+				if input[index] == ']' {
+					completed = true
+
+					break
+				}
+
+				name.WriteByte(input[index])
+
+				index++
+			}
+
+			index++
+
+			if !completed || name.Len() == 0 || index >= len(input) || input[index] != '(' {
+				continue
+			}
+
+			index++
+
+			var url strings.Builder
+
+			for index < len(input) {
+				if input[index] == ')' {
+					completed = true
+
+					break
+				}
+
+				url.WriteByte(input[index])
+
+				index++
+			}
+
+			index++
+
+			if !completed || url.Len() == 0 {
+				continue
+			}
+
+			ok := yield(MarkdownURL{
+				Name: name.String(),
+				URL:  url.String(),
+			})
+
+			if !ok {
+				return
+			}
+		}
+	}
 }
